a7240854f1b7d4be9aa49f8c840c33db7a3b6b14
EstherManrique <esmango0610@gmail.com>
2021-11-12 23:29:09 -0600

    Added new proyect AvBin

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -5,6 +5,70 @@ const Projects = () => {
     <Fragment>
       <div className="projects">
         <h1 className="gradient-title">Proyectos</h1>
+
+        <div className="project-item">
+          <div className="container">
+            <h3 className="text-centered">Avaluos de Bienes Inmuebles Maquinaria y Equipo</h3>
+            <p>
+              Aplicación diseñada para <strong> tablet (Ipad Pro) </strong> con el
+              objetivo de facilitar la comunicación entre meseros, cocina y
+              caja, optimizando tiempos para brindar un mejor servicio. La App
+              tiene dos roles de usuario: Meseros y Cocina.
+            </p>
+            <div className="grid">
+              <div className="project-objectives">
+                <h4 className="is-green">Objetivos:</h4>
+                <ol>
+                  <li>
+                    Tomar las ordenes a los comensales de forma rápida y
+                    eficiente.
+                  </li>
+                  <li>Agilizar la comunicación entre mesero, cocina y caja.</li>
+                </ol>
+              </div>
+              <div className="project-results">
+                <h4 className="is-green">Resultados:</h4>
+                <ol>
+                  <li>
+                    Menos margen de error en la preparación de los alimentos.
+                  </li>
+                  <li>Mayor exactitud al registrar el consumo en caja.</li>
+                  <li>Menos tiempo de espera para los comensales.</li>
+                </ol>
+              </div>
+            </div>
+            <div className="project-tech">
+              <h4>Tech:</h4>
+              <div className="project-tech-list">
+                Figma, HTML5, CSS3, Bootstrap, JavaScript, ReactJS, Firebase
+                (Firestore), Git &amp; Github (Projects, Issues, Labels).
+              </div>
+            </div>
+            <div className="project-image text-centered v-align-center">
+              <a
+                href="https://esthermanrique.github.io/CDMX009-BurgerQueen"
+                target="_blank "
+              >
+                <img src={process.env.PUBLIC_URL + "/assets/images/AvBin_Gif_OK.gif"} alt="" />
+              </a>
+            </div>
+            <div className="project-links">
+              <a
+                href="https://esthermanrique.github.io/CDMX009-BurgerQueen"
+                target="_blank "
+              >
+                <img src={process.env.PUBLIC_URL + "/assets/icons/enlace2.png"} alt="" />
+              </a>
+              <a
+                href="https://github.com/EstherManrique/CDMX009-BurgerQueen"
+                target="_blank "
+              >
+                <img src={process.env.PUBLIC_URL + "/assets/icons/github2.png"} alt="" />
+              </a>
+            </div>
+          </div>
+        </div>
+
         <div className="project-item">
           <div className="container">
             <h3 className="text-centered">Burger Queen</h3>
